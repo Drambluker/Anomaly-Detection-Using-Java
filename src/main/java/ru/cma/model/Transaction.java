4242f2a9ae6aed2a21244600a0284a2bb9246b6c
Drambluker <evlaskin@gmail.com>
2020-04-30 18:06:34 +0300

    Create Transaction.java
    For serialization/deserialization

---
+++ src/main/java/ru/cma/model/Transaction.java
@@ -0,0 +1,19 @@
+package ru.cma.model;
+
+public class Transaction {
+    String date;
+    String account;
+    Integer amount;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+}
