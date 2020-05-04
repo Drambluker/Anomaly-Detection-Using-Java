690bd6b697c79f017460ca272d6f2423e5d4171d
Drambluker <evlaskin@gmail.com>
2020-05-04 18:00:32 +0300

    Add detection by Isolation Forest

--- src/main/java/ru/cma/model/Transaction.java
+++ src/main/java/ru/cma/model/Transaction.java
@@ -1,5 +1,7 @@
 package ru.cma.model;
 
+import com.google.gson.annotations.Expose;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -8,6 +10,13 @@ public class Transaction {
     String date;
     String account;
     Integer amount;
+    boolean anomaly = false;
+
+    @Expose(serialize = false)
+    boolean isolationForestWarn = false;
+
+    @Expose(serialize = false)
+    boolean boxplotWarn = false;
 
     @XmlElement
     public String getDate() {
@@ -34,5 +43,32 @@ public class Transaction {
 
     public void setAmount(Integer amount) {
         this.amount = amount;
+    }
+
+    @XmlElement
+    public boolean isAnomaly() {
+        return anomaly;
+    }
+
+    public void setAnomaly(boolean anomaly) {
+        this.anomaly = anomaly;
+    }
+
+    public void setIsolationForestWarn(boolean isolationForestWarn) {
+        this.isolationForestWarn = isolationForestWarn;
+        updateAnomalyFlag();
+    }
+
+    public void setBoxplotWarn(boolean boxplotWarn) {
+        this.boxplotWarn = boxplotWarn;
+        updateAnomalyFlag();
+    }
+
+    private void updateAnomalyFlag() {
+        if (boxplotWarn && isolationForestWarn) {
+            anomaly = true;
+        } else {
+            anomaly = false;
+        }
     }
 }
--- src/main/java/ru/cma/utils/AnomalyDetectionTask.java
+++ src/main/java/ru/cma/utils/AnomalyDetectionTask.java
@@ -1,5 +1,10 @@
 package ru.cma.utils;
 
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.cma.ml.isolationForest.Classification;
+import ru.cma.ml.isolationForest.IsolationForestTrainer;
 import ru.cma.model.Transaction;
 
 import java.util.HashMap;
@@ -7,10 +12,33 @@ import java.util.List;
 import java.util.TimerTask;
 
 public class AnomalyDetectionTask extends TimerTask implements AnomalyDetector {
+    private static Logger log = LoggerFactory.getLogger(AnomalyDetectionTask.class.getSimpleName());
+
+    private final int BOXPLOT_CAPACITY;
+    private final int TRAINER_CAPACITY;
+
     HashMap<String, List<Transaction>> transactionByAccount;
+    IsolationForestTrainer trainer;
 
     public AnomalyDetectionTask(HashMap<String, List<Transaction>> transactionByAccount) {
         this.transactionByAccount = transactionByAccount;
+
+        //TODO Pick value
+        BOXPLOT_CAPACITY = 10;
+
+        TRAINER_CAPACITY = 250;
+    }
+
+    public AnomalyDetectionTask(HashMap<String, List<Transaction>> transactionByAccount, int boxPlotCapacity) {
+        this.transactionByAccount = transactionByAccount;
+        BOXPLOT_CAPACITY = boxPlotCapacity;
+        TRAINER_CAPACITY = 250;
+    }
+
+    public AnomalyDetectionTask(HashMap<String, List<Transaction>> transactionByAccount, int boxPlotCapacity, int trainerCapacity) {
+        this.transactionByAccount = transactionByAccount;
+        BOXPLOT_CAPACITY = boxPlotCapacity;
+        TRAINER_CAPACITY = trainerCapacity;
     }
 
     @Override
@@ -20,6 +48,54 @@ public class AnomalyDetectionTask extends TimerTask implements AnomalyDetector {
 
     @Override
     public void detect() {
-        //TODO Add some implementation
+        detectByBoxplot(BOXPLOT_CAPACITY);
+        detectByIsolationForest(TRAINER_CAPACITY);
+    }
+
+    public void detectByBoxplot(int capacity) {
+        //TODO Add implementation
+    }
+
+    private void detectByIsolationForest(int capacity) {
+        for (String key : transactionByAccount.keySet()) {
+            List<Transaction> transactions = transactionByAccount.get(key);
+
+            if (transactions.size() >= capacity) {
+                trainer = new IsolationForestTrainer(capacity);
+                addTrainingInstances(transactions, capacity);
+
+                try {
+                    trainer.trainModel();
+                    classifyTransactionsByTrainer(transactions, capacity);
+                } catch (Exception e) {
+                    log.error("Error while training and classification transactions by Isolation Forest", e);
+                }
+            }
+        }
+    }
+
+    private void addTrainingInstances(@NotNull List<Transaction> transactions, int capacity) {
+        for (int i = transactions.size() - capacity; i < transactions.size(); i++) {
+            Transaction transaction = transactions.get(i);
+
+            if (transaction.isAnomaly()) {
+                trainer.addTrainingInstances(transaction.getAmount(), Classification.ANOMALY);
+            } else {
+                trainer.addTrainingInstances(transaction.getAmount(), Classification.NORMAL);
+            }
+        }
+    }
+
+    private void classifyTransactionsByTrainer(@NotNull List<Transaction> transactions, int capacity) throws Exception {
+        for (int i = transactions.size() - capacity; i < transactions.size(); i++) {
+            Transaction transaction = transactions.get(i);
+            Classification classification = trainer.classify(transaction.getAmount());
+
+            if (classification == Classification.ANOMALY) {
+                transaction.setIsolationForestWarn(true);
+            } else {
+                transaction.setIsolationForestWarn(false);
+            }
+        }
     }
 }
